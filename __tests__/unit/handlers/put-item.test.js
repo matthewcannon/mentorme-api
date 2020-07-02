744390dbc3188cf12ebbded2b2fa5b488d2027b8
matthew.cannon@cdl.co.uk <matthew.cannon@cdl.co.uk>
2020-07-02 22:22:51 +0100

    Remove unused functions and refactor to improve naming.

--- __tests__/unit/handlers/put-item.test.js
+++ __tests__/unit/handlers/put-item.test.js
@@ -1,28 +1,19 @@
-// Import dynamodb from aws-sdk
 const dynamodb = require('aws-sdk/clients/dynamodb');
 
-// Import all functions from put-item.js
 const lambda = require('../../../src/handlers/put-item.js');
 
-// This includes all tests for putItemHandler
 describe('Test putItemHandler', () => {
     let putSpy;
 
-    // One-time setup and teardown, see more in https://jestjs.io/docs/en/setup-teardown
     beforeAll(() => {
-        // Mock DynamoDB put method
-        // https://jestjs.io/docs/en/jest-object.html#jestspyonobject-methodname
         putSpy = jest.spyOn(dynamodb.DocumentClient.prototype, 'put');
     });
 
-    // Clean up mocks
     afterAll(() => {
         putSpy.mockRestore();
     });
 
-    // This test invokes putItemHandler and compares the result
     it('should add id to the table', async () => {
-        // Return the specified value whenever the spied put function is called
         putSpy.mockReturnValue({
             promise: () => Promise.resolve('data'),
         });
@@ -32,14 +23,12 @@ describe('Test putItemHandler', () => {
             body: '{"id":"id1","name":"name1"}',
         };
 
-        // Invoke putItemHandler()
         const result = await lambda.putItemHandler(event);
         const expectedResult = {
             statusCode: 200,
             body: event.body,
         };
 
-        // Compare the result with the expected result
         expect(result).toEqual(expectedResult);
     });
 });
--- src/handlers/put-item.js
+++ src/handlers/put-item.js
@@ -1,30 +1,17 @@
-// Create clients and set shared const values outside of the handler
-
-// Create a DocumentClient that represents the query to add an item
 const dynamodb = require('aws-sdk/clients/dynamodb');
 
 const docClient = new dynamodb.DocumentClient();
 
-// Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
+const tableName = process.env.MENTORS_TABLE;
 
-/**
- * A simple example includes a HTTP post method to add one item to a DynamoDB table.
- */
 exports.putItemHandler = async (event) => {
     const { body, httpMethod, path } = event;
     if (httpMethod !== 'POST') {
         throw new Error(`postMethod only accepts POST method, you tried: ${httpMethod} method.`);
     }
-    // All log statements are written to CloudWatch by default. For more information, see
-    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-    console.log('received:', JSON.stringify(event));
 
-    // Get id and name from the body of the request
     const { id, name } = JSON.parse(body);
 
-    // Creates a new item, or replaces an old item with a new item
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
     const params = {
         TableName: tableName,
         Item: { id, name },
@@ -36,6 +23,5 @@ exports.putItemHandler = async (event) => {
         body,
     };
 
-    console.log(`response from: ${path} statusCode: ${response.statusCode} body: ${response.body}`);
     return response;
 };
